62cf4696db893f8d33e2236cd135204c80d25406
Jet Balagtas <jetbalagtas@gmail.com>
2015-10-21 22:20:56 -0400

    simultaneously doing html, sass, & jquery

---
+++ main.js
@@ -0,0 +1,58 @@
+$(document).ready(function () {
+
+
+var avatarURL =  "<img src='" + user.avatar_url + "'>";
+$('.avatar').append(avatarURL);
+
+$('.user').html(user.name);
+
+$('.username').html(user.login);
+
+$('.aboutLink').html(user.blog);
+
+$('.dateJoined').html(user.created_at);
+
+
+
+
+// var bitterHTML = "";
+//
+// _.each(bitterData, function (currVal, idx, arr) {
+//   bitterHTML += "<article class='bit'>"
+//     + "<h3>"
+//     + "<img src='"
+//     + currVal.avatar
+//     + "'>"
+//     + "</h3>"
+//     + "<p>"
+//     + currVal.bitter
+//     + "</p>"
+//     + "<button class='delete'>Delete</button>"
+//     + "</article>";
+//     console.log('bitterHTML: ', bitterHTML);
+// });
+//
+// $('.bitters').html(bitterHTML);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
